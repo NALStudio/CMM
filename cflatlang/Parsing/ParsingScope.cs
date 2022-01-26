5ccf64bd3d4101c82b036344f8c0ec55f2c2f3d3
NAL <niko.a.leinonen@gmail.com>
2022-01-26 08:22:04 +0200

    Update ParsingScope.cs

--- cflatlang/Parsing/ParsingScope.cs
+++ cflatlang/Parsing/ParsingScope.cs
@@ -10,7 +10,23 @@ namespace cflatlang.Parsing;
 
 internal class ParsingScope
 {
-    public Dictionary<string, IEnumerable<LexingToken>> Functions = new();
+    public Dictionary<string, List<LexingToken>> Functions = new();
     public Dictionary<string, string> Constants = new();
     public Dictionary<string, string> Variables = new();
+
+    public ParsingScope Copy()
+    {
+        ParsingScope ps = new();
+
+        foreach ((string name, List<LexingToken> tokens) in Functions)
+            ps.Functions.Add(name, tokens);
+
+        foreach ((string name, string value) in Constants)
+            ps.Constants.Add(name, value);
+
+        foreach ((string name, string value) in Variables)
+            ps.Variables.Add(name, value);
+
+        return ps;
+    }
 }
