c64c3861e95fe58a70ba6d15a5a65f0a282ed782
NAL <niko.a.leinonen@gmail.com>
2022-02-07 21:28:42 +0200

    Create IntermediateInstruction.cs

---
+++ cflatlang/Intermediate/IntermediateInstruction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cflatlang.Intermediate;
+
+internal enum IntermediateInstruction
+{
+    /// <summary>
+    /// Load a variable onto the stack.
+    /// </summary>
+    Load,
+
+    /// <summary>
+    /// Push a value onto the stack.
+    /// </summary>
+    Push,
+
+    /// <summary>
+    /// Pop a value from the stack into a variable.
+    /// </summary>
+    Store,
+
+    // TODO: Rest and also investigate how these should be interpreted.
+}
